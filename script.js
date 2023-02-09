3a909ca2acde25d983c2018b3d3a7af18dd8940d
DanielJHV <120484672+DanielJHV@users.noreply.github.com>
2023-02-10 00:14:15 +0200

    Add decimal point button functionality

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let currentNumber = 0;
 let firstNumber = 0;
 let secondNumber = 0;
 let isAdding = false;
-let isSubstracting = false; 
+let isSubstracting = false;
 let isMultiplying = false;
 let isDividing = false;
 
 function resetOperators() {
   isAdding = false;
-  isSubstracting = false; 
+  isSubstracting = false;
   isMultiplying = false;
   isDividing = false;
   firstNumber = 0;
@@ -21,7 +21,7 @@ function equals() {
   if (isAdding === true) {
     currentNumber = firstNumber + secondNumber;
     screenNumber.textContent = currentNumber;
-    resetOperators()
+    resetOperators();
   } else if (isSubstracting === true) {
     currentNumber = +(firstNumber - secondNumber);
     screenNumber.textContent = currentNumber;
@@ -38,7 +38,12 @@ function equals() {
 }
 
 function calculatePercentage() {
-  if (isAdding === true || isSubstracting === true || isMultiplying === true || isDividing === true) {
+  if (
+    isAdding === true ||
+    isSubstracting === true ||
+    isMultiplying === true ||
+    isDividing === true
+  ) {
     currentNumber = firstNumber / 100;
     equals();
   } else {
@@ -67,7 +72,7 @@ document.querySelector(".add-btn").addEventListener("click", () => {
   firstNumber = currentNumber;
   currentNumber = 0;
   isAdding = true;
-}); 
+});
 
 document.querySelector(".substract-btn").addEventListener("click", () => {
   firstNumber = currentNumber;
@@ -91,62 +96,70 @@ document.querySelector(".percent-btn").addEventListener("click", () => {
   calculatePercentage();
 });
 
+document.querySelector(".point-btn").addEventListener("click", () => {
+  if (typeof currentNumber == "string" || currentNumber % 1 != 0) {
+    return;
+  } else {
+    currentNumber += ".";
+    screenNumber.textContent = currentNumber;
+  }
+});
 document.querySelector(".zero-btn").addEventListener("click", () => {
   currentNumber += "0";
-  currentNumber = + currentNumber;
+  currentNumber = +currentNumber;
   screenNumber.textContent = currentNumber;
 });
 
 document.querySelector(".one-btn").addEventListener("click", () => {
   currentNumber += "1";
-  currentNumber = + currentNumber;
+  currentNumber = +currentNumber;
   screenNumber.textContent = currentNumber;
 });
 
 document.querySelector(".two-btn").addEventListener("click", () => {
   currentNumber += "2";
-  currentNumber = + currentNumber;
+  currentNumber = +currentNumber;
   screenNumber.textContent = currentNumber;
 });
 
 document.querySelector(".three-btn").addEventListener("click", () => {
   currentNumber += "3";
-  currentNumber = + currentNumber;
+  currentNumber = +currentNumber;
   screenNumber.textContent = currentNumber;
 });
 
 document.querySelector(".four-btn").addEventListener("click", () => {
   currentNumber += "4";
-  currentNumber = + currentNumber;
+  currentNumber = +currentNumber;
   screenNumber.textContent = currentNumber;
 });
 
 document.querySelector(".five-btn").addEventListener("click", () => {
   currentNumber += "5";
-  currentNumber = + currentNumber;
+  currentNumber = +currentNumber;
   screenNumber.textContent = currentNumber;
 });
 
 document.querySelector(".six-btn").addEventListener("click", () => {
   currentNumber += "6";
-  currentNumber = + currentNumber;
+  currentNumber = +currentNumber;
   screenNumber.textContent = currentNumber;
 });
 
 document.querySelector(".seven-btn").addEventListener("click", () => {
   currentNumber += "7";
-  currentNumber = + currentNumber;
+  currentNumber = +currentNumber;
   screenNumber.textContent = currentNumber;
 });
 
 document.querySelector(".eight-btn").addEventListener("click", () => {
   currentNumber += "8";
-  currentNumber = + currentNumber;
+  currentNumber = +currentNumber;
   screenNumber.textContent = currentNumber;
 });
 
 document.querySelector(".nine-btn").addEventListener("click", () => {
   currentNumber += "9";
-  currentNumber = + currentNumber;
+  currentNumber = +currentNumber;
   screenNumber.textContent = currentNumber;
 });
